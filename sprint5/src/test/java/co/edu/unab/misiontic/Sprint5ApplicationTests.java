58c038234f64732a32c10e95ce8b44fdb14b5c6f
Diego López B <89640777+Saint-Diego@users.noreply.github.com>
2021-10-17 17:32:11 -0500

    Backend del sprint 5

---
+++ sprint5/src/test/java/co/edu/unab/misiontic/Sprint5ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unab.misiontic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sprint5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
